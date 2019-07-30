bd4ab203c07fd5a20e0d03b17132ed2e39e55bdd
hongikhs <sw@hongik.hs.kr>
2019-07-31 07:56:40 +0900

    Update motor_pwm_test.py

--- motor_pwm_test.py
+++ motor_pwm_test.py
@@ -4,12 +4,12 @@ from orangepwm import OrangePwm
 
 gpio.init()
 
-le = port.PA21		# ENA
-lb = port.PA18		# IN1
-lf = port.PG8		  # IN2
-rb = port.PG9		  # IN3
-rf = port.PG6		  # IN4
-re = port.PG7		  # ENB
+re = port.PA21		# ENA
+rb = port.PA18		# IN1
+rf = port.PG8		# IN2
+lf = port.PG9		# IN3
+lb = port.PG6		# IN4
+le = port.PG7		# ENB
 
 lp = OrangePwm(100, le)
 rp = OrangePwm(100, re)
@@ -18,20 +18,24 @@ gpio.setcfg(lb, 1)
 gpio.setcfg(rf, 1)
 gpio.setcfg(rb, 1)
 
-lp.start(100)
-gpio.output(lf, 1)
+gpio.output(lf, 1)	# gpio.HIGH : 1
 gpio.output(lb, 0)	# gpio.LOW : 0
-gpio.output(re, 1)	# gpio.HIGH : 1
+gpio.output(rb, 0)
 gpio.output(rf, 1)
 
-lp.start(70)
-rp.start(100)
-
+print('40% Power')
+lp.start(40)
+rp.start(40)
 sleep(1)
 
-lp.changeDutyCycle(100)
+print('70% Power')
+lp.changeDutyCycle(70)
 rp.changeDutyCycle(70)
+sleep(1)
 
+print('100% Power')
+lp.changeDutyCycle(100)
+rp.changeDutyCycle(100)
 sleep(1)
 
 lp.stop()
