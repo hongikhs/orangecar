9582c227a9437854ee76e894c2a31802b46f5df6
hongikhs <sw@hongik.hs.kr>
2019-08-03 00:01:11 +0900

    Update camshift_track.py

--- camshift_track.py
+++ camshift_track.py
@@ -8,7 +8,7 @@ ret,frame = cap.read()
  
 # setup initial location of window
 #r,h,c,w = 250,90,400,125  # simply hardcoded the values
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
+r,h,c,w =150,100,200,150  # simply hardcoded the values
 track_window = (c,r,w,h)
  
 # set up the ROI for tracking
